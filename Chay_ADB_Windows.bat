68142c314f0a6ad0b9d229b9066f2e3615fe7119
levuvinhkhang737373 <levuvinhkhang737373@gmail.com>
2026-07-21 23:50:32 +0700

    nop bai

--- Chay_ADB_Windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Khoi dong ADB Server cho WSL - StayHub
-echo [StayHub] Dang khoi dong ADB Server tren Windows de lien ket voi WSL...
-cd /d "C:\Users\Martyr\AppData\Local\Packages\32533HUXSoft.APKInstallerandManagerforWindows_aep6pg3hkma3e\LocalState\platform-tools"
-adb.exe kill-server
-adb.exe -a nodaemon server start
-pause
